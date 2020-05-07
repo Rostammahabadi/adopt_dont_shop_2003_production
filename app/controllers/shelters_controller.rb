e2293a55707d39870f556dd6de6ba0bdbd3fa9b5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-07 15:38:52 -0600

    Add ability to update shelters

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -22,4 +22,21 @@ class SheltersController < ApplicationController
         shelter.save
         redirect_to "/shelters"
     end
+
+    def edit
+        @shelter = Shelter.find(params[:id])
+    end
+
+    def update
+        shelter = Shelter.find(params[:id])
+        shelter.update(shelter_params)
+        shelter.save
+        redirect_to "/shelters/#{shelter.id}"
+    end
+
+    private
+
+    def shelter_params
+        params.permit(:name, :address, :city, :state, :zip)
+    end
 end
