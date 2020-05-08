3e4e995f5a08012a85b95c62e32794a80241b7ca
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-07 20:24:07 -0600

    refactor new.html.erb to use ActionView helpers

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -12,14 +12,7 @@ class SheltersController < ApplicationController
     end
 
     def create
-        shelter = Shelter.new({
-        name: params[:shelter][:name],
-        address: params[:shelter][:address],
-        city: params[:shelter][:city],
-        state: params[:shelter][:state],
-        zip: params[:shelter][:zip]
-        })
-        shelter.save
+        shelter = Shelter.create(shelter_params)
         redirect_to "/shelters"
     end
 
