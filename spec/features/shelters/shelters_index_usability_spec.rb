39299f40e32e65274aba1029d84b46b6c118eb72
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-08 23:19:00 -0600

    Add links for shelter names on shelter index page

--- spec/features/shelters/shelters_index_usability_spec.rb
+++ spec/features/shelters/shelters_index_usability_spec.rb
@@ -32,6 +32,18 @@ describe "When I visit the shelter index Page", type: :feature do
 
         expect(current_path).to eq("/shelters")
         expect(page).to_not have_content("#{shelter_1.name}")
+    end
 
+    it "has shelter names as clickable links to their show page" do
+        shelter_1 = Shelter.create( name: "4 Paws Rescue",
+                    address: "6567 W Long Dr.",
+                    city: "Littleton",
+                    state: "CO",
+                    zip: "80123")
+
+        visit "/shelters"
+        click_link("#{shelter_1.name}")
+
+        expect(current_path).to eq("/shelters/#{shelter_1.id}")
     end
 end
