892063396bce06fca6eb5e838280d09daaf34c51
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-08 23:53:01 -0600

    Add links for pet names

--- spec/features/pets/pets_index_usability_spec.rb
+++ spec/features/pets/pets_index_usability_spec.rb
@@ -95,4 +95,29 @@ describe "When I visit the pet index or shelter pets Page", type: :feature do
         click_link("#{pet_1.shelter.name}")
         expect(current_path).to eq("/shelters/#{shelter_1.id}")
     end
+
+    it "has links for pet names" do
+        shelter_1 = Shelter.create( name: "4 Paws Rescue",
+                    address: "6567 W Long Dr.",
+                    city: "Littleton",
+                    state: "CO",
+                    zip: "80123")
+
+        pet_1 = Pet.create( name:    "Teddy",
+                            image: "https://i.pinimg.com/originals/47/4e/7d/474e7d2479512428a1a4716d5d1656eb.jpg",
+                            age:    "10",
+                            sex:   "M",
+                            description: "A sweet dog looking for a loving home",
+                            shelter_id: shelter_1.id,
+                            adoption_status: "Adoptable")
+        visit("/pets")
+        click_link "#{pet_1.name}"
+
+        expect(current_path).to eq("/pets/#{pet_1.id}")
+
+        visit("/shelters/#{shelter_1.id}/pets")
+        click_link "#{pet_1.name}"
+
+        expect(current_path).to eq("/pets/#{pet_1.id}")
+    end
 end
