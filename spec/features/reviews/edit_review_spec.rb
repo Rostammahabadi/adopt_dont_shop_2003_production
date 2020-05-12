f62189533d4a6b8db90179daa5621e6abb739485
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-12 12:08:25 -0600

    uncomment test and made more robust by testing sad path

--- spec/features/reviews/edit_review_spec.rb
+++ spec/features/reviews/edit_review_spec.rb
@@ -30,23 +30,25 @@ RSpec.describe "Edit a review", type: :feature do
     expect(page).to have_content("Great")
 
   end
-  # it "flashes message when trying to update without title, rating or content" do
-  #   shelter1 = Shelter.create(
-  #     name: "Puppies",
-  #     address: "123 west street",
-  #     city: "Scottsdale",
-  #     state: "AZ",
-  #     zip: 85257
-  #   )
-  #   review1 = shelter1.reviews.create(
-  #     title: "Awesome",
-  #     rating: 4,
-  #     content: "This place was amazing, really cared for their animals",
-  #     picture: "url.url"
-  #   )
-  #   visit "/shelters/#{shelter1.id}/reviews/#{review1.id}/edit"
-  #   fill_in "title", with: ''
-  #   click_on("Update")
-  # end
+  it "flashes message when trying to update without title, rating or content" do
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+    )
+    review1 = shelter1.reviews.create(
+      title: "Awesome",
+      rating: 4,
+      content: "This place was amazing, really cared for their animals",
+      picture: "url.url"
+    )
+    visit "/shelters/#{shelter1.id}/reviews/#{review1.id}/edit"
+    fill_in "title", with: ''
+    click_on("Update")
+    expect(page).to have_content("Need to enter title, rating, and review in order to update.")
+    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/#{review1.id}/edit")
+  end
 
 end
