9e918a2594558190794fc5f451ca28600d7a7031
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-11 21:53:36 -0600

    Add spec for incomplete review form

--- spec/features/reviews/create_review_spec.rb
+++ spec/features/reviews/create_review_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe "New review page", type: :feature do
         expect(page).to have_content("5")
         expect(page).to have_content("Very clean place with happy pets.")
     end
+
+    it "will not create a review id required fields are missing" do
+        shelter_1 = Shelter.create( name: "4 Paws Rescue",
+                            address: "6567 W Long Dr.",
+                            city: "Littleton",
+                            state: "CO",
+                            zip: "80123")
+        visit "/shelters/#{shelter_1.id}"
+        click_link("Write a Review")
+        expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
+
+        fill_in :title, with: "Great Shelter"
+        select('5', from: :rating)
+        click_button "Submit"
+
+        expect(page).to have_content("Review not posted: You must fill in the Title, Rating, and Content in order to post a review.")
+        expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
+        expect(page).to have_button("Submit")
+    end
 end
