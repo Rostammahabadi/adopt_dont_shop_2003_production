1609694add078dab57e0f6909498cc71dc3e0b7b
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-11 21:48:37 -0600

    added in code from Kyle's commit including new and create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,15 @@
 class ReviewsController < ApplicationController
+
+def new
+      @shelter_id = params[:shelter_id]
+  end
+
+  def create
+      shelter = Shelter.find(params[:shelter_id])
+      shelter.reviews.create(review_params)
+      redirect_to "/shelters/#{shelter.id}"
+  end
+
   def edit
     @review = Review.find(params[:review_id])
   end
