d3f47a962365b9fe5dbae5631a733972b9f9e7de
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-13 11:20:53 -0600

    keep the @shelter_id accessible when rendering new in review controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :favorites
-  
+
   def favorites
     @favorites ||= Favorites.new(session[:favorites])
+  end
+
+  before_action :new
+
+  private
+
+  def shelter_id
+    @shelter_id = params[:shelter_id]
   end
 end
