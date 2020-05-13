5baa495dca0d407272065430572cd36529825cc0
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-12 22:14:54 -0600

    remove redundant line from FavoritesController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,6 @@
 class FavoritesController < ApplicationController
     def update
         pet = Pet.find(params[:pet_id])
-        pet_id_string = pet.id.to_s
         favorites.add_pet(pet.id)
         session[:favorites] = favorites.contents
         flash[:notice] = "#{pet.name} has been added to favorites."
