760d6e312affea35faf89a801543b56936272df4
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-14 11:59:24 -0600

    add test for no favorites page

---
+++ spec/features/favorites/no_favorites_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe "User can see no favorites on page when favorites is zero" do
+    it "displays no pets when no favorites" do
+        visit("/favorites")
+        expect(page).to have_content("No Favorited Pets")
+    end
+end
