ffc7f18c96818b719778dd4e5ced126de38f33a2
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-14 15:00:01 -0600

    Add favorite_class and favorite_text methods and tests

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,20 @@
 class Pet < ApplicationRecord
     validates_presence_of :name, :image, :description, :age, :sex, :adoption_status
     belongs_to :shelter
+
+    def favorite_text(favorites)
+        if favorites.include?(id)
+            "Remove from Favorites"
+        else
+            "Favorite"
+        end
+    end
+
+    def favorite_class(favorites)
+        if favorites.include?(id)
+            "remove-favorite"
+        else
+            "shelter-update-top"
+        end
+    end
 end
