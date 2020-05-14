430fc146062e5be77df4d26aed05e9282a2c8494
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-14 16:23:11 -0600

    Add test for updating favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
     def update
         pet = Pet.find(params[:pet_id])
-        favorites.add_pet(pet.id)
+        favorites.update_pet(pet.id)
         session[:favorites] = favorites.contents
         flash[:notice] = "#{pet.name} has been added to favorites."
         redirect_to("/pets/#{pet.id}")
--- spec/features/favorites/update_favorite_spec.rb
+++ spec/features/favorites/update_favorite_spec.rb
@@ -87,4 +87,48 @@ RSpec.describe "When a user adds pets to their cart" do
             expect(page).to have_link("Remove from Favorites")
         end
     end
+
+    it "decreases the favorites counter when Remove from Favorites is clicked" do
+        shelter_1 = Shelter.create( name:    "4 Paws Rescue",
+                                    address: "6567 W Long Dr.",
+                                    city:    "Littleton",
+                                    state:   "CO",
+                                    zip:     "80123")
+
+
+        pet_1 = Pet.create( name:    "Teddy",
+                                    image: "https://i.pinimg.com/originals/47/4e/7d/474e7d2479512428a1a4716d5d1656eb.jpg",
+                                    age:    "10",
+                                    sex:   "M",
+                                    description: "A sweet dog looking for a loving home",
+                                    shelter_id: shelter_1.id,
+                                    adoption_status: "Adoptable")
+
+        pet_2 = Pet.create( name:    "Spot",
+                                    image: "https://i.pinimg.com/originals/47/4e/7d/474e7d2479512428a1a4716d5d1656eb.jpg",
+                                    age:    "10",
+                                    sex:   "M",
+                                    description: "A dog looking for a loving home",
+                                    shelter_id: shelter_1.id,
+                                    adoption_status: "Adoptable")
+
+        visit "/shelters"
+        expect(page).to have_content("Favorites: 0")
+
+        visit "/pets/#{pet_1.id}"
+        click_on "Favorite"
+        visit "/pets/#{pet_2.id}"
+        click_on "Favorite"
+
+        within("nav") do
+            expect(page).to have_content("Favorites: 2")
+        end
+
+        visit "/pets/#{pet_2.id}"
+        click_on "Remove from Favorites"
+
+        within("nav") do
+            expect(page).to have_content("Favorites: 1")
+        end
+    end
 end
