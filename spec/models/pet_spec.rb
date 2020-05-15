cba16ad718a61f3276e905371c3dc15f674feb25
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-15 16:54:17 -0600

    Add pet_application and application relationships to pet class

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -12,6 +12,8 @@ describe Pet, type: :model do
 
     describe "relationships" do
         it {should belong_to :shelter}
+        it {should have_many :pet_applications}
+        it {should have_many(:applications).through((:pet_applications))}
     end
 
     it "creates an instance of Pet" do
