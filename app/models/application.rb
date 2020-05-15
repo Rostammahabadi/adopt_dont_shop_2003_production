31dce273529d48ce86336b565713cc120bbc2093
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-15 14:57:41 -0600

    update application to plural

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ApplicationRecord
   has_many :pet_applications
-  has_many :pets, through: :pet_application
+  has_many :pets, through: :pet_applications
   validates_presence_of :name
   validates_presence_of :address
   validates_presence_of :city
