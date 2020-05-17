e4367c604bb7fcca56748700fb75d8e5ed37e517
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-17 13:26:03 -0600

    Add link_class and link_text to PetApplication class

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,20 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def link_text
+    if approved
+      "Unapprove"
+    else
+      "Approve"
+    end
+  end
+
+  def link_class
+    if approved
+      "remove-favorite"
+    else
+      "shelter-update-top"
+    end
+  end
 end
