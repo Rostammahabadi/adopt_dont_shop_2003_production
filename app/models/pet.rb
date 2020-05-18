a092b64e285a2b0d236410673de74dfbccc3d462
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-17 18:26:41 -0600

    Move default values for pet to the model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,10 @@
 class Pet < ApplicationRecord
-    validates_presence_of :name, :image, :description, :age, :sex, :adoption_status
+    validates_presence_of :name, :image, :description, :age, :sex
     belongs_to :shelter
     has_many :pet_applications
     has_many :applications, through: :pet_applications
+
+    before_save :default_values
 
     def favorite_text(favorites)
         if favorites.include?(id)
@@ -26,5 +28,9 @@ class Pet < ApplicationRecord
         else
             :patch
         end
+    end
+
+    def default_values
+        self.adoption_status ||= 'Adoptable'
     end
 end
